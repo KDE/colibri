aaf135895934068920446986470deeb40d50bbc7
Aurélien Gâteau <agateau@kde.org>
2013-06-05 14:50:45 +0200

    Add test for duplicated notifications

---
+++ tests/duplicate.sh
@@ -0,0 +1,7 @@
+# If an app emits several notifications with the same summary and body, Colibri
+# should ignore the duplicates
+notify-send -i user "Spammer" "Buy my stuff!"
+sleep 1
+notify-send -i user "Spammer" "Buy my stuff!"
+sleep 1
+notify-send -i user "Spammer" "Buy my stuff!"
